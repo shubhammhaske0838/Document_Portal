a8def9d8d88e12a38d1255241f18cc33c2ff550c
shubham mhaske <shubham.mhaske0838@gmail.com>
2025-07-27 02:02:21 +0530

    Project Setup and Notebook Experiment Completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="document_portal",
+    author="Shubham Mhaske",
+    version='0.1',
+    packages=find_packages()    # List your package dependencies here
+    
+)
+
